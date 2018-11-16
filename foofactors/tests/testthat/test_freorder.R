a7b0d30e68bc579110b211c10c9a052466749766
yukirang <rangk@163.com>
2018-11-15 23:48:19 -0800

    finishing

--- foofactors/tests/testthat/test_freorder.R
+++ foofactors/tests/testthat/test_freorder.R
@@ -1,12 +1,12 @@
 context("Reorder factors")
 
 test_that("freorder reorders factors in descendence", {
-  x <- c("4", "2", "3")
-  y <- c(7, 9, 8)
+  x <- c("4","2","3")
+  y <- c("2", "2", "3")
   z <- data.frame(x,y)
   x_re <- c("4","3","2")
-  y_re <- c(9,8,7)
-  expect_identical(freorder(factor(x),x_re))
-  expect_identical(freorder(factor(y),y_re))
+  y_re <- c("3","2")
+  expect_identical(freorder(factor(x)),x_re)
+  expect_identical(freorder(factor(y)),y_re)
   expect_error(freorder(z))
 })
